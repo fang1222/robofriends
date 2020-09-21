690b47f638ee6c447799b9536dbf1e136d95313d
fangyu <hsufangyu1222@gmail.com>
2020-09-21 17:21:47 +0800

    update

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import Cardlist from './Cardlist';
 import Search from './Search';
 import {robots} from './robots';
+import './App.css';
 
 const App = () => {
   return(
     <div className='tc' >
-      <h1>Robofriends</h1>
+      <h1 className='f1'>Robofriends</h1>
       <Search />
       <Cardlist robots={robots} />
     </div>
