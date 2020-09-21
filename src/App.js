ab1ec8910e0498cb6b7f14f303d632aad812d4cf
fangyu <hsufangyu1222@gmail.com>
2020-09-21 11:50:28 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,28 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Cardlist from './Cardlist';
+import Search from './Search';
+import {robots} from './robots';
 
-class App extends React.Component {
-  render () {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+const App = () => {
+  return(
+    <div className='tc' >
+      <h1>Robofriends</h1>
+      <Search />
+      <Cardlist robots={robots} />
+    </div>
+  );
 }
 
 export default App;
+
