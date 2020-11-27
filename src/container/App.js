f18248f881945d1eb84dfd1654d63ff28c30b809
fangyu <hsufangyu1222@gmail.com>
2020-11-27 13:33:31 +0800

    move in container, component files

---
+++ src/container/App.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import Cardlist from '../component/Cardlist';
+import Search from '../component/Search';
+import Scroll from '../component/Scroll';
+import './App.css';
+
+class App extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      robots: [],
+      searchfield: '' 
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({robots: users}));
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ searchfield : event.target.value })
+  }
+
+  render () {
+    const { robots, searchfield } = this.state;
+    const filteredRobots = robots.filter(robot => {
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase()) ;
+    });
+    
+    return !robots.length ? 
+    <h1>loading...</h1> : 
+    (
+      <div className='tc' >
+        <h1 className='f1'>Robofriends</h1>
+        <Search searchChange={this.onSearchChange} />
+        <Scroll>
+          <Cardlist robots={filteredRobots} />
+        </Scroll>
+      </div>
+    );
+  }
+}
+
+export default App;
+
